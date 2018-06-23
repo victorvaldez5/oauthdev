e3f109296abd914bbb9ae0b9d8990b1d69084f05
Victor Valdez <victorvaldez5@gmail.com>
2018-06-22 20:16:17 -0500

    init commit

---
+++ server/auth/init.js
@@ -0,0 +1,15 @@
+var passport = require('passport');
+var User = require('../models/user');
+
+module.exports = function() {
+  passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(function(user, done) {
+    User.findById(id, function (err, user) {
+      done(err, user);
+    });
+  });
+
+};
